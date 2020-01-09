c287a40a68eb2ee897a8c4879898500e1f6aded5
Luis Joel Caballero <luijo@outlook.com>
2020-01-09 13:18:45 -0600

    Cliking on any breed button shows 10 result still images with its rating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,7 @@ function breedButtons(breedsArray, newClass, newArea){
 };
 
 $(document).on("click",".breedSearch", function(){
+    $("#searches").empty();
     var breed = $(this).data("type");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q="+breed+"&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9"+"&limit=10"
     $.ajax({
@@ -24,5 +25,21 @@ $(document).on("click",".breedSearch", function(){
         method: "GET"
       }).then(function(response) {
         console.log(response);
+        for(i=0;i<response.data.length;i++){
+            var searchDiv = $("<div class='search-item'>");
+            var rating = response.data[i].rating;
+            var p = $("<p>").text("Rating: "+rating);
+            var animated = response.data[i].images.fixed_height.url;
+            var still = response.data[i].images.fixed_height_still.url;
+            var image = $("<img>");
+            image.attr("src", still);
+            image.attr("data-still", still);
+            image.attr("data-animated", animated);
+            image.attr("data-state","still");
+            image.addClass("searchImage");
+            searchDiv.append(p);
+            searchDiv.append(image);
+            $("#results").append(searchDiv);
+        }
       });
 });

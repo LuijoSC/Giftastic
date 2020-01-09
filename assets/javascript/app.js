653e4926571a64ed56b0f753cd1f7cc8c4aa8179
Luis Joel Caballero <luijo@outlook.com>
2020-01-09 12:22:17 -0600

    Clicking on any breed returns an object of 10 Giphy results on console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ $(function(){
 
 var breedsArray= ["Pug", "Shiba Inu", "Dachshund"];
 
-function breedButtons(breeds, newClass, newArea){
+function breedButtons(breedsArray, newClass, newArea){
     $(newArea).empty();
     for(var i=0;i<breedsArray.length; i++){
         var a= $("<button>");
@@ -15,3 +15,14 @@ function breedButtons(breeds, newClass, newArea){
         $(newArea).append(a);
     }
 };
+
+$(document).on("click",".breedSearch", function(){
+    var breed = $(this).data("type");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+breed+"&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9"+"&limit=10"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      });
+});
